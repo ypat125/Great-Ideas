fbe29ecc8956808dbb143bf1ca6507efeea14ff8
Yash Patil <yash.s.patil125@gmail.com>
2019-01-23 14:32:44 -0600

    Initial Commit

---
+++ Assets/CharController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 4f; //Change in inspector to adjust move speed
+    Vector3 forward, right; // Keeps track of our relative forward and right vectors
+
+    public int forceConst = 50;
+
+    private bool canJumpPressed;
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+
+    void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody>();
+        forward = Camera.main.transform.forward; // Set forward to equal the camera's forward vector
+        forward.y = 0; // make sure y is 0
+        forward = Vector3.Normalize(forward); // make sure the length of vector is set to a max of 1.0
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // set the right-facing vector to be facing right relative to the camera's forward vector
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        canJump = true;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        canJump = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (canJumpPressed && canJump)
+        {
+            canJumpPressed = false;
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            canJumpPressed = true;
+        }
+        if (Input.anyKey) // only execute if a key is being pressed
+            Move();
+    }
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); // setup a direction Vector based on keyboard input. GetAxis returns a value between -1.0 and 1.0. If the A key is pressed, GetAxis(HorizontalKey) will return -1.0. If D is pressed, it will return 1.0
+        transform.position += direction/moveSpeed; // move our transform's position right/left
+    }
+}
---
+++ Assets/colorChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour {
+    // Interpolate light color between two colors back and forth
+    public float duration = 1.0f;
+    Color color0 = Color.red;
+    Color color1 = Color.blue;
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // set light color
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+    }
+}
