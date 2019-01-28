261b199762e1d7334c6abc85639a9790329f2b31
Yash Patil <yash.s.patil125@gmail.com>
2019-01-28 09:56:38 -0600

    Initial Commit

---
+++ Assets/instantiate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiate : MonoBehaviour {
+
+    public Transform prefab;
+    void Start()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        while (true) {
+            yield return new WaitForSeconds(3);
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Assets/spawnSpikes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnSpikes : MonoBehaviour {
+
+    public Transform prefab;
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        Vector3 SpawnHere = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Instantiate(prefab, SpawnHere, other.transform.rotation);
+        Destroy(gameObject);
+    }
+}
